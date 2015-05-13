96c32c3989dca13cebfe35ffbc39e1fc0bf1d207
Georce <wujian@wujian360.cn>
2015-05-13 17:38:05 +0800

    4001

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 ETCD_MACHINES=${ETCD_MACHINES:-"$ETCD_HOST"}
 
-etcdctl -C $ETCD_HOST:4001 --no-sync set /skydns/local/$DOMAIN/"`hostname -s `" '{"host":"'`ip a | grep "scope global eth0" | grep -o '\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}'`'"}'
+etcdctl -C $ETCD_HOST --no-sync set /skydns/local/$DOMAIN/"`hostname -s `" '{"host":"'`ip a | grep "scope global eth0" | grep -o '\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}'`'"}'
 
 skydns
