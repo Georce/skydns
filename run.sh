d97fcf7ac01e8cf6c5ea8ca4bbb85f735ad3cdbc
Georce <wujian@wujian360.cn>
2015-05-15 11:51:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 etcdctl -C $ETCD_HOST --no-sync set /skydns/local/$DOMAIN/"`hostname -s `" '{"host":"'`ip a | grep "scope global eth0" | grep -o '\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}'`'"}'
 
-skydns -machines=$ETCD_HOST
+skydns
